c61eb831757dfa4e51a78a3a9a849b71e1cb911a
rr250 <rr250@snu.edu.in>
2019-11-29 17:55:09 +0530

    Updates

---
+++ precache-manifest.a8ae29c80d2b1c0503a3c0ddd89180ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2407e8aa50309e8135094b7958e5a08",
+    "url": "/FractalAnalyticsTodoApp/index.html"
+  },
+  {
+    "revision": "d59c704671e2f4188404",
+    "url": "/FractalAnalyticsTodoApp/static/css/main.91f1a783.chunk.css"
+  },
+  {
+    "revision": "3a77eae7e4fd98d53731",
+    "url": "/FractalAnalyticsTodoApp/static/js/2.750895d2.chunk.js"
+  },
+  {
+    "revision": "d59c704671e2f4188404",
+    "url": "/FractalAnalyticsTodoApp/static/js/main.47ee3fb4.chunk.js"
+  },
+  {
+    "revision": "31ecbe81cb4140446f06",
+    "url": "/FractalAnalyticsTodoApp/static/js/runtime-main.9d671e00.js"
+  }
+]);
